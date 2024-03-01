b954a5f06156fb533e9c59ef5f1589cdf0dc24f6
RedDuo <emailtest@gmail.com>
2024-03-01 17:03:31 +0100

    lol, 1 proba

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("WOWOWOWOWOWOWOWOWOWOW");
+            Console.WriteLine("WOWOWOWOWOWOWOWOasWOWOW");
         }
     }
 }
