908c2df1c247acbc3132a2a4f085ce20de572c38
RedDuo <emailtest@gmail.com>
2024-03-01 17:06:00 +0100

    druga zmiana

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("WOWOWOWOWOWOWOWOasWOWOW");
+            Console.WriteLine("Robimy gita, wszystko rozumiem");
         }
     }
 }
